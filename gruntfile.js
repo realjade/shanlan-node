459af110e3b151a53fc839008e6681e9f5d74e8c
zhangyy-g <zhangyy-g@grandsoft.com.cn>
2014-09-10 18:13:45 +0800

    commit

--- gruntfile.js
+++ gruntfile.js
@@ -3,23 +3,36 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      concat: {
+          options: {
+              separator: ';',
+          },
+          dist: {
+              src: [
+                  'static/js/libs/jquery/jquery.js', 
+                  'static/js/libs/backbone/underscore.js',
+                  'static/js/libs/backbone/backbone.js',
+                  'static/js/libs/mustache.js'
+                  ],
+              dest: 'static/js/min/libs.js',
+          },
       },
-      build: {
-        src: 'static/js/jspass.js',
-        dest: 'static/js/min/jspass.js'
+      uglify: {
+        build: {
+          src: 'static/js/min/libs.js',
+          dest: 'static/js/min/libs.min.js'
+        }
       }
-    }
   });
 
+  // 加载包含 "concat" 任务的插件。
+  grunt.loadNpmTasks('grunt-contrib-concat');
   // 加载包含 "uglify" 任务的插件。
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  //执行压缩合并Lib任务
+  grunt.registerTask('libs', ['concat', 'uglify']);
+
   // 默认被执行的任务列表。
   //grunt.registerTask('default', ['uglify']);
-
-  grunt.registerTask('test', ['uglify']);
-
 };
