831441b581711aa8138dea934e2135dd55ab50fe
jadezhang <zyyjade@gmail.com>
2014-09-21 12:52:49 +0800

    commit

--- config_template.js
+++ config_template.js
@@ -1,9 +1,22 @@
+/**
+ * @name zhangyy-g@grandsoft.com.cn
+ * @description zhangyy's code
+ * Date: 2014/9/11
+ * Time: 21:44
+ *
+ */
 var config = {
-    javaServer: 'https://yun.glodon.com',
-    javaPort: '80',
+    env: 'development',
+    javaServer: '182.92.159.195',
+    javaPort: '8080',
+    //javaPort: '443', //https
 
     redis: {
-        host: '',
-        port: ''
+        host: "127.0.0.1",
+        port: 6379,
+        db: "0",
+        prefix: "user-"
     }
 };
+
+module.exports = config
