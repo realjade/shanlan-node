725c53c5df3774dc292d9394be9e6fd64d9fbb8a
jadezhang <zyyjade@gmail.com>
2014-09-28 23:12:22 +0800

    commit

--- static/js/module/module.imageview.js
+++ static/js/module/module.imageview.js
@@ -4,5 +4,22 @@
  */
 
 (function($){
+    var imageView = {
+        init: function(container, options){
+
+        }
+    }
+
+    imageView.__tpl = '' +
+        '<div class="mod-image-view">' +
+        '   <em class="prev"></em>' +
+        '   <div class="image-panel"></div>' +
+        '   <em class="next"></em>' +
+        '   <div class="count-panel">' +
+        '   ' +
+        '   </div>' +
+        '</div>'
+
+    App.common.modules.imageView = imageView
 
 })(jQuery)
