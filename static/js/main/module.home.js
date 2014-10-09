338a23aea056f6b316a7d0854e1b58da992ff6e3
jadezhang <zyyjade@gmail.com>
2014-10-09 23:47:41 +0800

    commit

---
+++ static/js/main/module.home.js
@@ -0,0 +1,18 @@
+/**
+ * @name zhangyy-g@grandsoft.com.cn
+ * @description zhangyy's code
+ * Date: 14-10-9
+ * Time: 下午11:45
+ *
+ */
+(function($){
+    var home = {
+        init: function(container){
+            
+        }
+    }
+
+    $(function(){
+        home.init($('.mod-home'))
+    })
+})(jQuery)
