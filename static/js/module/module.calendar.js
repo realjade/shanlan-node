554e4e2fd5ef688cadd60141fc82c6a369f957e1
jadezhang <zyyjade@gmail.com>
2014-10-21 23:48:04 +0800

    conmit

--- static/js/module/module.calendar.js
+++ static/js/module/module.calendar.js
@@ -3,26 +3,30 @@
  * description: dialog
  */
 
-(function($){
+(function ($) {
     var calendar = {
 
         __container: null,
 
         __options: null,
 
-        init: function(container, options){
+        init: function (container, options) {
             var self = this
             self.__container = container
             self.__options = $.extend({
-                weeks : [ '周日', '周一', '周二', '周三', '周四', '周五', '周六' ],
-                weekabbrs : [ '周日', '周一', '周二', '周三', '周四', '周五', '周六' ],
-                months : [ '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月' ],
-                monthabbrs : [ '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月' ],
-                onDayClick: $.noop,
-                onDayHover: $.noop,
+                weeks: [ '周日', '周一', '周二', '周三', '周四', '周五', '周六' ],
+                weekabbrs: [ '周日', '周一', '周二', '周三', '周四', '周五', '周六' ],
+                months: [ '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月' ],
+                monthabbrs: [ '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月' ],
+                onDayClick: function($el, $content, dateProperties){
+                    self.__dayClick($el, $content, dateProperties)
+                },
+                onDayHover: function($el, $content, dateProperties){
+                    self.__dayHover($el, $content, dateProperties)
+                },
                 caldata: '',
                 displayWeekAbbr: true,
-                selectedDay: []
+                selectedDay: {}
             }, options)
 
             var cal = self.__calendar = container.find('#calendar').calendario(self.__options);
@@ -32,7 +36,7 @@
             return cal
         },
 
-        __initHeader: function(){
+        __initHeader: function () {
             var self = this
             var container = self.__container
             var calendar = self.__calendar
@@ -40,17 +44,27 @@
             var month = $('#custom-month', container).html(calendar.getMonthName());
             var year = $('#custom-year', container).html(calendar.getYear());
 
-            $( '#custom-next', container ).on( 'click', function() {
-                calendar.gotoNextMonth( updateMonthYear );
-            } );
-            $( '#custom-prev', container ).on( 'click', function() {
-                calendar.gotoPreviousMonth( updateMonthYear );
-            } );
+            $('#custom-next', container).on('click', function () {
+                calendar.gotoNextMonth(updateMonthYear);
+            });
+            $('#custom-prev', container).on('click', function () {
+                calendar.gotoPreviousMonth(updateMonthYear);
+            });
 
             function updateMonthYear() {
-                month.html( calendar.getMonthName() );
-                year.html( calendar.getYear() );
+                month.html(calendar.getMonthName());
+                year.html(calendar.getYear());
             }
+        },
+
+        __dayClick: function($el, $content, dateProperties){
+            if(!$el.hasClass('fc-past') && !$el.hasClass(' fc-selected')){
+                $el.toggleClass('fc-checked')
+            }
+        },
+
+        __dayHover: function($el, $content, dateProperties){
+
         }
     }
 
--- static/js/profile/module.package.js
+++ static/js/profile/module.package.js
@@ -6,94 +6,94 @@
  *
  */
 
-(function($){
+(function ($) {
     var profilePackage = {
 
         __container: null,
 
-        init: function(container){
+        init: function (container) {
             var self = this
             self.__container = container
 
             var calendar = App.common.modules.calendar.init($('.custom-calendar-wrap', container), {
-                onDayClick: function(){
-
+                selectedDay: {
+                    1413907200000: true
                 }
             })
         },
 
-        __calendarClick: function(){
+        __calendarClick: function () {
 
         }
     }
 
-    $(function(){
+    $(function () {
         profilePackage.init($('.mod-package'))
         App.common.modules.profileLayout.init($('.mod-profile-header-wrap'))
         /* Calendar */
         /*var transEndEventNames, $wrapper, $calendar, cal, $month, $year;
-        transEndEventNames = {
-            'WebkitTransition': 'webkitTransitionEnd',
-            'MozTransition': 'transitionend',
-            'OTransition': 'oTransitionEnd',
-            'msTransition': 'MSTransitionEnd',
-            'transition': 'transitionend'
-        };
-        $wrapper = $('#custom-inner');
-        $calendar = $('#calendar');
-        cal = $calendar.calendario({
-            onDayClick: function ($el, $contentEl, dateProperties) {
+         transEndEventNames = {
+         'WebkitTransition': 'webkitTransitionEnd',
+         'MozTransition': 'transitionend',
+         'OTransition': 'oTransitionEnd',
+         'msTransition': 'MSTransitionEnd',
+         'transition': 'transitionend'
+         };
+         $wrapper = $('#custom-inner');
+         $calendar = $('#calendar');
+         cal = $calendar.calendario({
+         onDayClick: function ($el, $contentEl, dateProperties) {
 
-                if ($contentEl.length > 0) {
-                    showEvents($contentEl, dateProperties);
-                }
+         if ($contentEl.length > 0) {
+         showEvents($contentEl, dateProperties);
+         }
 
-            },
-            caldata: '',
-            displayWeekAbbr: true
-        });
-        $month = $('#custom-month').html(cal.getMonthName());
-        $year = $('#custom-year').html(cal.getYear());
+         },
+         caldata: '',
+         displayWeekAbbr: true
+         });
+         $month = $('#custom-month').html(cal.getMonthName());
+         $year = $('#custom-year').html(cal.getYear());
 
-        $( '#custom-next' ).on( 'click', function() {
-            cal.gotoNextMonth( updateMonthYear );
-        } );
-        $( '#custom-prev' ).on( 'click', function() {
-            cal.gotoPreviousMonth( updateMonthYear );
-        } );
+         $( '#custom-next' ).on( 'click', function() {
+         cal.gotoNextMonth( updateMonthYear );
+         } );
+         $( '#custom-prev' ).on( 'click', function() {
+         cal.gotoPreviousMonth( updateMonthYear );
+         } );
 
-        function updateMonthYear() {
-            $month.html( cal.getMonthName() );
-            $year.html( cal.getYear() );
-        }
+         function updateMonthYear() {
+         $month.html( cal.getMonthName() );
+         $year.html( cal.getYear() );
+         }
 
-        // just an example..
-        function showEvents( $contentEl, dateProperties ) {
+         // just an example..
+         function showEvents( $contentEl, dateProperties ) {
 
-            hideEvents();
+         hideEvents();
 
-            var $events = $( '<div id="custom-content-reveal" class="custom-content-reveal"><h4>Events for ' + dateProperties.monthname + ' ' + dateProperties.day + ', ' + dateProperties.year + '</h4></div>' ),
-                $close = $( '<span class="custom-content-close"></span>' ).on( 'click', hideEvents );
+         var $events = $( '<div id="custom-content-reveal" class="custom-content-reveal"><h4>Events for ' + dateProperties.monthname + ' ' + dateProperties.day + ', ' + dateProperties.year + '</h4></div>' ),
+         $close = $( '<span class="custom-content-close"></span>' ).on( 'click', hideEvents );
 
-            $events.append( $contentEl.html() , $close ).insertAfter( $wrapper );
+         $events.append( $contentEl.html() , $close ).insertAfter( $wrapper );
 
-            setTimeout( function() {
-                $events.css( 'top', '0%' );
-            }, 25 );
+         setTimeout( function() {
+         $events.css( 'top', '0%' );
+         }, 25 );
 
-        }
-        function hideEvents() {
+         }
+         function hideEvents() {
 
-            var $events = $( '#custom-content-reveal' );
-            if( $events.length > 0 ) {
+         var $events = $( '#custom-content-reveal' );
+         if( $events.length > 0 ) {
 
-                $events.css( 'top', '100%' );
-                Modernizr.csstransitions ? $events.on( transEndEventName, function() { $( this ).remove(); } ) : $events.remove();
+         $events.css( 'top', '100%' );
+         Modernizr.csstransitions ? $events.on( transEndEventName, function() { $( this ).remove(); } ) : $events.remove();
 
-            }
+         }
 
-        }*/
-})
+         }*/
+    })
 
 
 })(jQuery)
