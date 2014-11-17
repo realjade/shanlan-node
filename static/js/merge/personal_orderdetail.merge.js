275471d215f35f07b9311c4779c7572d6f0ce9c3
刘鹏 <liupenggood@gmail.com>
2014-11-17 02:43:00 +0000

    后台管理-订单详情

---
+++ static/js/merge/personal_orderdetail.merge.js
@@ -0,0 +1,12 @@
+/**
+ * @name Edwin
+ * @description
+ * Date: 2014/10/19
+ * Time: 23:18
+ *
+ */
+
+document.write('<script src="//static.mgcheng.com/static/js/common/module.common.js"></script>')
+document.write('<script src="//static.mgcheng.com/static/js/common/module.personallayout.js"></script>')
+document.write('<script src="//static.mgcheng.com/static/js/module/module.dialog.js"></script>')
+document.write('<script src="//static.mgcheng.com/static/js/personal/module.orderdetail.js"></script>')
