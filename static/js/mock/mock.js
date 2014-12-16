caf92fd465fb3ac87c42efa38ae1530bf170364e
zyy <zhangyuyu_1987@qq.com>
2014-12-16 21:11:29 +0800

    commi

--- static/js/mock/mock.js
+++ static/js/mock/mock.js
@@ -7,33 +7,10 @@
  */
 $(function() {
 
-    // Setup html5 version
-    $("#uploader").pluploadQueue({
-        // General settings
-        runtimes : 'html5,flash,html4',
-        url : '/opf/upload/uploadPhotos?collectionId=1',
-        //chunk_size: '1mb',
-        rename : true,
-        dragdrop: true,
+    var upload = new App.common.modules.upload($("#uploader"), {
+        url: '/upload'
+    })
 
-        filters : {
-            // Maximum file size
-            max_file_size : '20mb',
-            // Specify what files to browse for
-            mime_types: [
-                {title : "Image files", extensions : "jpg,gif,png"},
-                {title : "Zip files", extensions : "zip"}
-            ]
-        },
-
-        /*multipart_params:{
-            collectionId: '1'
-        },*/
-
-        // Resize images on clientside if we can
-        //resize : {width : 320, height : 240, quality : 90},
-
-        flash_swf_url : '//static.mgcheng.com/static/js/libs/jquery/plupload/js/Moxie.swf'
-    });
+    upload.setUrl('/opf/upload/uploadPhotos?collectionId=1')
 
 });
